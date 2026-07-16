623c8c9e913d73ad016bb01874be1ca61fa71100
unknown <stefanpopovicnew@gmail.com>
2026-07-16 20:57:21 +0200

    Add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react'
 import './theme.css';
+
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header/Header'
-import Hero from './pages/Hero/Hero'
-import TrustBar from './pages/TrustBar/TrustBar'
-import AboutClinic from './pages/AboutClinic/AboutClinic';
-import Treatments from './pages/Treatments/Treatments';
-import Process from './pages/Process/Process';
-import Results from './pages/Results/Results';
-import Offer from './pages/Offer/Offer';
-import VideoShowcase from './pages/VideoShowcase/VideoShowCase';
-import Pricing from './pages/Pricing/Pricing';
-import Testimonials from './pages/Testimonials/Testimonials';
-import FinalCta from './pages/FinalCta/Finalcta';
+import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 import StickyBar from './components/Stickybar/Stickybar';
 
@@ -19,20 +12,18 @@ function App() {
 
   return (
     <>
+
       <Header />
-      <Hero />
-      <TrustBar/>
-      <AboutClinic/>
-      <Treatments />
-      <Pricing/>
-      <Process/>
-      <Results />     
-      <Offer />
-      <VideoShowcase/>
-      <Testimonials/>
-      <FinalCta/>
-      <Footer/>
-      <StickyBar/>
+
+      <Routes>
+
+          <Route path='/' element={ <Home />}></Route>
+
+      </Routes>
+
+        <Footer />
+        <StickyBar />
+
     </>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+
+import Hero from "../Hero/Hero";
+import TrustBar from "../TrustBar/TrustBar";
+import AboutClinic from "../AboutClinic/AboutClinic";
+import Treatments from "../Treatments/Treatments";
+import Pricing from "../Pricing/Pricing";
+import Process from "../Process/Process";
+import Results from "../Results/Results";
+import Offer from "../Offer/Offer";
+import VideoShowcase from "../VideoShowcase/VideoShowCase";
+import Testimonials from "../Testimonials/Testimonials";
+import FinalCta from "../FinalCta/Finalcta";
+
+
+function Home() {
+
+  return (
+    <>
+      <Hero />
+      <TrustBar/>
+      <AboutClinic/>
+      <Treatments />
+      <Pricing/>
+      <Process/>
+      <Results />     
+      <Offer />
+      <VideoShowcase/>
+      <Testimonials/>
+      <FinalCta/>
+    </>
+  )
+}
+
+export default Home
